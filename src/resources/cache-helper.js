886e7e5b3be13bf9bc5ac418595fab88054134cc
Alex Nicholson <alex@serieseight.com>
2024-01-11 13:57:08 +0000

    Added function to reinitialise

--- src/resources/cache-helper.js
+++ src/resources/cache-helper.js
@@ -101,3 +101,7 @@ function getCookie(name) {
 	const parts = value.split(`; ${name}=`);
 	if (parts.length === 2) return parts.pop().split(';').shift();
 }
+
+window.dynamicFormsInit = function() {
+	init();
+}
