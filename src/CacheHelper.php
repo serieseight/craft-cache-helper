ec0fed9aaf32faaa3fd390d3b3de82d63215f9ed
Alex Nicholson <alex@serieseight.com>
2024-01-11 12:25:33 +0000

    Use nystudio107 way of loading CP templates

--- src/CacheHelper.php
+++ src/CacheHelper.php
@@ -5,6 +5,8 @@ namespace serieseight\cachehelper;
 use Craft;
 use craft\base\Plugin;
 use serieseight\cachehelper\assetbundles\CacheHelperBundle;
+use craft\web\View;
+use craft\helpers\Template;
 
 /**
  * Cache Helper plugin
@@ -13,7 +15,7 @@ use serieseight\cachehelper\assetbundles\CacheHelperBundle;
  */
 class CacheHelper extends Plugin
 {
-    public string $schemaVersion = '1.0.0';
+    public string $schemaVersion = '1.0.1';
 
     public function init(): void
     {
@@ -31,10 +33,38 @@ class CacheHelper extends Plugin
             Craft::$app->view->registerAssetBundle(CacheHelperBundle::class);
 
             Craft::$app->getView()->hook('formie.form.end', function(array &$context) {
-                return $context['view']->renderTemplate('_cache-helper/_caching.twig', [
+                return $this->renderPluginTemplate('_caching.twig', [
                     'form' => $context['form'],
                 ]);
             });
         }
+    }
+
+    protected function renderPluginTemplate($templatePath, $vars)
+    {
+        // Stash the old template mode, and set it Control Panel template mode
+        $oldMode = Craft::$app->view->getTemplateMode();
+        try {
+            Craft::$app->view->setTemplateMode(View::TEMPLATE_MODE_CP);
+        } catch (Exception $e) {
+            Craft::error($e->getMessage(), __METHOD__);
+        }
+
+        // Render the template with our vars passed in
+        try {
+            $htmlText = Craft::$app->view->renderTemplate('_cache-helper/'.$templatePath, $vars);
+        } catch (\Exception $e) {
+            $htmlText = 'Error rendering template '.$templatePath.' -> '.$e->getMessage();
+            Craft::error(Craft::t('_cache-helper', $htmlText), __METHOD__);
+        }
+
+        // Restore the old template mode
+        try {
+            Craft::$app->view->setTemplateMode($oldMode);
+        } catch (Exception $e) {
+            Craft::error($e->getMessage(), __METHOD__);
+        }
+
+        return Template::raw($htmlText);
     }
 }
